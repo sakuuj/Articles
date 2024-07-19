85ac13705dda8ec82d6186e84e0e5dbbac81ac16
Pavel Rysnik <pavelrysnik@gmail.com>
2024-07-20 01:55:22 +0300

    feat: add integration tests plugin

---
+++ buildSrc/src/main/kotlin/int-test.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("java")
+}
+
+sourceSets {
+    create("intTest") {
+        compileClasspath += sourceSets.main.get().output
+        compileClasspath += sourceSets.test.get().output
+
+        runtimeClasspath += sourceSets.main.get().output
+    }
+}
+
+configurations["intTestImplementation"].extendsFrom(configurations.testImplementation.get())
+configurations["intTestRuntimeOnly"].extendsFrom(configurations.testRuntimeOnly.get())
+configurations["intTestAnnotationProcessor"].extendsFrom(configurations.testAnnotationProcessor.get())
+configurations["intTestCompileOnly"].extendsFrom(configurations.testCompileOnly.get())
+
+val integrationTest = task<Test>("intTest") {
+    description = "Runs integration tests."
+    group = "verification"
+
+    testClassesDirs = sourceSets["intTest"].output.classesDirs
+    classpath = sourceSets["intTest"].runtimeClasspath
+    mustRunAfter("test")
+
+    useJUnitPlatform()
+
+    testLogging {
+        events("passed")
+    }
+}
+
+tasks.check { dependsOn(integrationTest) }
