5cab3fa693e9d7297fe4bb4ad606c568bd158f6f
Pavel Rysnik <pavelrysnik@gmail.com>
2024-07-20 02:03:17 +0300

    feat: configure RestClient to use virtual threads properly

---
+++ services/article-service/src/main/java/by/sakuuj/blogplatform/article/configs/RestClientConfig.java
@@ -0,0 +1,39 @@
+package by.sakuuj.blogplatform.article.configs;
+
+import by.sakuuj.blogplatform.article.utils.CompileTimeConstants;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
+
+import java.net.http.HttpClient;
+import java.util.concurrent.Executor;
+
+@Configuration(proxyBeanMethods = false)
+public class RestClientConfig {
+
+    @Bean
+    public RestClient restClient(ClientHttpRequestFactory requestFactory) {
+        return RestClient.builder()
+                .requestFactory(requestFactory)
+                .build();
+    }
+
+    @Bean
+    public ClientHttpRequestFactory clientHttpRequestFactory(HttpClient httpClient) {
+        return new JdkClientHttpRequestFactory(httpClient);
+    }
+
+    @Bean
+    public HttpClient httpClient(@Qualifier(CompileTimeConstants.EXECUTOR_BEAN_NAME)
+                                 Executor executor) {
+        return HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_1_1)
+                .executor(executor)
+                .build();
+    }
+
+
+}
---
+++ services/article-service/src/main/java/by/sakuuj/blogplatform/article/utils/CompileTimeConstants.java
@@ -0,0 +1,5 @@
+package by.sakuuj.blogplatform.article.utils;
+
+public class CompileTimeConstants {
+    public static final String EXECUTOR_BEAN_NAME = "taskExecutor";
+}
